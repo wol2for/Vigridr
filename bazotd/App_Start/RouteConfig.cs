a1c7fb0e4e0c9f45418dbd36285681917b6645e7
Freeman Hugh <wol2for@hotmail.com>
2017-09-07 20:29:51 +0300

    bbb

--- bazotd/App_Start/RouteConfig.cs
+++ bazotd/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace bazotd
 
             routes.MapRoute(
                 name: "Default",
-                url: "",
+                url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
